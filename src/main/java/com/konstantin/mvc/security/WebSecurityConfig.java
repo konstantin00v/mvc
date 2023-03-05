574338625f4b8c674287fd4b737d2f714f28e1ad
Konstantin <renevan@list.ru>
2023-03-05 12:33:33 +0300

    Added role system and secured methods

--- src/main/java/com/konstantin/mvc/security/WebSecurityConfig.java
+++ src/main/java/com/konstantin/mvc/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.konstantin.mvc.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.*;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,31 +11,34 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
         auth.inMemoryAuthentication()
-                .withUser("Ivan")
-                .password("1234")
+                .withUser("user")
+                .password("123")
                 .roles("USER")
+
                 .and()
                 .withUser("admin")
-                .password("admin")
+                .password("123")
                 .roles("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-//                .antMatchers("/api/").hasRole("USER")
-//                .antMatchers("/api/").hasRole("ADMIN")
-                .antMatchers("/api").permitAll()
-                .and().formLogin();
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/**").hasAnyRole("USER", "ADMIN")
+                .and().formLogin().permitAll();
+
     }
 
     @Bean
-    public PasswordEncoder encoder () {
+    public PasswordEncoder encoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 
